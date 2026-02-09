31413398c530c25b931a53e56ce953cede812ac6
angiegranda <143025114+angiegranda@users.noreply.github.com>
2026-02-09 16:16:49 +0100

    Add server proxy for /images and /api

--- ecommerce-frontend/vite.config.js
+++ ecommerce-frontend/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // is the request starts with /api the server will be target to this default url
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000'
+      },
+      '/images': {
+        target: 'http://localhost:3000'
+      }
+    }
+  }
 })
