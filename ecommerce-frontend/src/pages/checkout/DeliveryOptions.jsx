2daa55049644d5bc620bc3ed218355456a0efb48
angiegranda <143025114+angiegranda@users.noreply.github.com>
2026-02-11 17:37:35 +0100

    Finished generating HTML for orders page

---
+++ ecommerce-frontend/src/pages/checkout/DeliveryOptions.jsx
@@ -0,0 +1,32 @@
+import dayjs from 'dayjs'
+import { formatMoney } from "../../utils/Money";
+
+export function DeliveryOptions( { cartItem, deliveryOptions } ) {
+  return (
+    <div className="delivery-options">
+      <div className="delivery-options-title">
+        Choose a delivery option:
+      </div>
+      {deliveryOptions.map((deliveryOption) => {
+        let priceString = deliveryOption.priceCents <= 0 ?
+          "FREE Shipping" :
+          `$${formatMoney(deliveryOption.priceCents)} - Shipping`;
+        return (
+          <div key={deliveryOption.id} className="delivery-option">
+            <input type="radio" checked={deliveryOption.id === cartItem.deliveryOptionId}
+              className="delivery-option-input"
+              name={`delivery-option-${cartItem.productId}`} />
+            <div>
+              <div className="delivery-option-date">
+                {dayjs(deliveryOption.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
+              </div>
+              <div className="delivery-option-price">
+                {priceString}
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
